f327404ef988e3c3fcee28abe3ef1c19e37c8ee6
kvdeshmuk31 <kvdeshmuhkh31@gmail.com>
2022-03-10 18:13:15 +0530

    commit1

---
+++ excercise.py
@@ -0,0 +1,62 @@
+#name = input("Enter your full name: ")
+# i=0
+# temp =""
+# while i <  len(name) :
+#     if name[i] not in temp:
+#         temp += name[i] 
+#         print(f"{name[i]}:{name.count(name[i])}")
+#     i += 1
+# for i in name:
+#     #print(f"{name}: {name.count(char)}")
+#     print(f"{name[i]:name.count(char)}")
+
+
+#string addition while loop
+# number= input("Enter a number like 1234: ") 
+# total=0
+# i = 0
+# while i < len(number):
+#      total += int(number[i])
+#      i += 1
+# print(total)
+
+# n=input("Enter natural number: ")
+# n = int(n)
+# total = 0
+# i=1
+# while i<=n:
+#     total += i
+#     i+=1
+# print(total)
+
+# user_input =input("Enter natural numbers: ")
+# n=int(user_input)
+# i=1
+# while i <= n:
+#     print(n)
+#     i+=1
+
+# n= input("Enter number: ")
+# n= int(n)
+# total=0
+# i=1
+# while i <= n:
+#     total+=i
+#     i+=1
+# print(total)
+# name = input("Enter your name: ")
+# age= int(input("enter yor age: "))
+# if age > 10 and (name[0] == "a" or name[0]== 'A'):
+#     print("you can watch movie")
+# else:
+#     print("sorry you cannot watch coco")
+# #Number guessing game
+# winning_number = 9 
+# guess_number =int(input("enter guess number: "))
+
+# if guess_number == winning_number:
+#     print("You WIN!!!!!")
+# if guess_number < winning_number:
+#     print("Too low")
+# if guess_number > winning_number:
+#     print("Too high")
--- hello.py
+++ hello.py
@@ -1,1 +1,18 @@
-print("HELLO WORLD")
+# name, char = input("Enter your name and letter: ").split(",")
+# print(f"Length of your name is: {len(name)}")
+# print(f"count is: {name.count(char)}")
+# print(f"Length of your name is {name} {letter} ")
+#===========================================================
+# print("This is \\\\ double backsash")
+# print("These are /\/\/\/\ mountains")
+# print("He is\tawesome")
+# print("\\\" \\n \\t \\'")
+#===========================================================
+# num1 =input("Enter num1: ")
+# num2 =input("Enter num2: ")
+# num3 =input("Enter num2: ")
+# num1,num2,num3=input("enter three number: ").split()
+# avg = (int(num1)+int(num2)+int(num3))/3
+# print(f"avg of number is {avg} ")
+# name = input("Enter user name: ")
+# print(name[::-1])
